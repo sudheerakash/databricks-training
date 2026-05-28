b8f10315c0fa4b82a13719afa968a50a33f68979
Kalam Sudheer Akash <sudheerakash07@gmail.com>
2026-05-28 23:23:38 +0530

    Fix comment punctuation in Schema.sql
    Corrected punctuation in the comment.

--- Week1/day2/Schema.sql
+++ Week1/day2/Schema.sql
@@ -1,4 +1,4 @@
-Below is the code for creating a table and insert the data into the table
+Below is the code for creating a table and insert the data into the table.
 CREATE TABLE Employees (
     emp_id INT,
     emp_name VARCHAR(50),
